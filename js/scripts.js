5cfb699341ea1b80dd886f5875e48267fbc1212a
leevee6039 <leevee6039@gmail.com>
2022-04-11 20:54:32 +0530

    added pokemonList array in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
-//alert() function
-alert('Hello world!');
-
-// added variable favoriteFood
-let favoriteFood = 'Chicken Biryani';
-document.write(favoriteFood);
+// array list of pokemon
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 7,
+    types: ['grass', 'posion']
+  },
+  {
+    name: 'Charmander',
+    height: 6,
+    types: 'fire'
+  },
+  {
+    name: 'Squirtle',
+    height: 5,
+    types: 'water'
+  },
+  {
+    name: 'Weedle',
+    height: 6,
+    types: ['bug', 'posion']
+  },
+  {
+    name: 'Pikachu',
+    height: 4,
+    types: 'electric'
+  }
+];
