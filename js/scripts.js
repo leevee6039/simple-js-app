04c8b038c61de876b8a9e08a6418b056233cf3a5
leevee6039 <leevee6039@gmail.com>
2022-04-21 13:12:11 +0530

    updated js file added loop for the array and output in the browser + added styles + updated html file added favicon and font

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,15 @@ let pokemonList = [
     types: ['electric']
   }
 ];
+
+let pokemon = ''; // declare a empty variable
+ // for loop that iterates each pokemon in the above array
+for(let i = 0; i < pokemonList.length; i++) {
+  // added condition for the pokemon height > 5
+  if (pokemonList[i].height > 5) {
+    pokemon = `<p class='pokemon-list'>${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!</p>`;
+    document.write(pokemon);
+  } else {
+    document.write(`<p class='pokemon-list'>${pokemonList[i].name} (height: ${pokemonList[i].height})</p>`);
+  }
+}
